9e090a33b2eec39cae57aae64742644bb53924e0
xkevinx45 <kneely1995@gmail.com>
2018-03-20 18:51:51 -0400

    Finish upoading

--- quadratic/Program.cs
+++ quadratic/Program.cs
@@ -6,7 +6,21 @@ namespace quadratic
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double a, b, c, x1, x2;
+
+            Console.WriteLine("What is a?");
+            a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("What is b?");
+            b = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("What is c?");
+            c = double.Parse(Console.ReadLine());
+
+            x1 = ((-b) + Math.Sqrt((b * b) - 4 * a * c)) / (2 * a);
+            x2 = ((-b) - Math.Sqrt((b * b) - 4 * a * c)) / (2 * a);
+
+           Console.WriteLine("The answer is: {0}, {1}", x1, x2);
         }
     }
 }
